b0c61783715d353631ce70bf4a2dc3e22593f6e8
admin <840180960@qq.com>
2019-07-16 05:59:04 +0800

    更新代码

--- git-test-project/src/com/han/git/gitTest.java
+++ git-test-project/src/com/han/git/gitTest.java
@@ -3,8 +3,10 @@ package com.han.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git project!");
+        //你好我是注释
     }
     public void test(){
         System.out.println("项目第二次修改");
+        System.out.println("新增加代码");
     }
 }
