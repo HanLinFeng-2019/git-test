8cac1412b2b7d9750a0e4e232926fdeb2996e93a
admin <840180960@qq.com>
2019-07-16 05:39:52 +0800

    第一次修改项目

---
+++ git-test-project/src/com/han/git/gitTest.java
@@ -0,0 +1,10 @@
+package com.han.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git project!");
+    }
+    public void test(){
+        System.out.println("项目第一次修改");
+    }
+}
