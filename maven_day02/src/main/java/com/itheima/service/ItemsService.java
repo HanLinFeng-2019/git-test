5e72b135f5f2a77236f9c991e7cd72a2ebe8660f
admin <840180960@qq.com>
2019-07-16 05:19:54 +0800

    删除

--- maven_day02/src/main/java/com/itheima/service/ItemsService.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.domain.Items;
-
-public interface ItemsService {
-
-    Items findById(Integer Id);
-}
--- maven_day02_parent/maven_day02_web/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-<a href="items/controller">测试</a>
-</body>
-</html>
